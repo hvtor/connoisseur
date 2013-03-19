2a71781aaf6c5a4578e7baa27369779134dd076d
Hemant V. Torsekar <your_email@example.com>
2013-03-19 13:03:40 -0400

    First commit.

--- html_generator.rb
+++ html_generator.rb
@@ -1,3 +1,56 @@
+require 'json'
+require 'open-uri'
+
 class HtmlGenerator
 
+	def show 
+		print_header
+		puts "Action: Show"
+
+		product = retrieve_data("http://lcboapi.com/products/#{id}.json")
+		print_footer
+	end
+
+	def index
+		print_header
+		puts "Action: Index"
+
+		products = retrieve_data
+		products.each do |x| 
+			display_product(x)
+		end
+		print_footer
+	end
+
+	private 
+
+	def display_product(product)
+		puts "<h1 align:left>#{product['name']}</h1>"
+		puts "<img src=#{product['image_thumb_url']}"
+		puts "<ul style="none">"
+		puts "<li>#{product['id']}</li>"
+		puts "<li>#{product['producer_name']}</li>"
+		puts "<li>#{product['primary_category']}</li>"
+		puts "<li>#{product['secondary_category']}</li>"
+		puts "<li>$#{(product['price_in_cents']*0.01/product['total_package_units']).float}</li>"
+		puts "</ul>"
+	ends
+
+	def print_header
+		puts "<html>"
+		puts "   <head>"
+		puts "   </head>"
+		puts "</html>"
+	end	
+
+	def print_footer
+    	puts "  </body>"
+    	puts "</html>"
+  	end
+
+	def retrieve_data()
+		response = open("http://lcboapi.com/products.json").read
+		data = JSON.parse(response)
+		return data["result"]
+	end
 end
--- router.rb
+++ router.rb
@@ -5,15 +5,15 @@ require_relative "html_generator"
   if ARGV.empty? #(argument vector)
     puts "USAGE: bla bla bla"
   else
-    puts "Ya, you passed in some arguments. :)"
+    
     generator = HtmlGenerator.new
     if ARGV[0] == "index"
-        generator.index
-    
+      generator.index
     elsif ARGV[0] == "show"
-      puts "Action: show"
-        generator.show
+      id = ARGV[1]
+      generator.show(id)
     else
       puts "Urecognized action. Please refer to USAGE."
     end
   end
+puts ARGV.inspect
